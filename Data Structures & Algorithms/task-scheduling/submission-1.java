2d3fbe9a7148e40cc9aaab4489219bf0b661b3f2
mrhhsmr <mrhhs123@gmail.com>
2026-05-02 18:03:05 -0700

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] count = new int [26];
+        for(int i = 0 ; i < tasks.length ; i++){
+            count[tasks[i] - 'A']++;
+        }
+
+        var pq = new PriorityQueue<Integer>((a, b) -> (b - a));
+        for (int ct : count){
+            if (ct > 0){
+                pq.offer(ct);
+            }
+        }
+
+        int time = 0;
+
+        while(!pq.isEmpty()){
+            List<Integer> temp = new ArrayList<>();
+            int cycle = n + 1;
+
+            while(!pq.isEmpty() && cycle > 0){
+                int currTask = pq.poll();
+                if(currTask > 1){
+                    temp.add(currTask - 1);
+                }
+                cycle--;
+                time++;
+            }
+
+            for(int nextTask : temp){
+                pq.offer(nextTask);
+            }
+
+            if(pq.isEmpty()){
+                break;
+            }
+
+            time += cycle;
+        }
+
+        
+        return time;
+    }
+}
