5a0d0ac9f522ec9cdee319f6b53c19873b55532c
mrhhsmr <mrhhs123@gmail.com>
2026-05-03 23:46:52 -0700

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.py
@@ -0,0 +1,40 @@
+class TrieNode:
+
+    def __init__(self):
+        self.kids = [None] * 26
+        self.end = False
+    
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            i = ord(c) - ord('a')
+            if cur.kids[i] is None:
+                cur.kids[i] = TrieNode()
+            cur = cur.kids[i]
+        cur.end = True
+        
+
+    def search(self, word: str) -> bool:
+        def dfs(index, root):
+            cur = root
+
+            for i in range(index, len(word)):
+                if word[i] == '.':
+                    for kid in cur.kids:
+                        # search not none kid
+                        if kid is not None and dfs(i + 1, kid):
+                            return True
+                    return False
+                else:
+                    nextIdx = ord(word[i]) - ord('a')
+                    if cur.kids[nextIdx] is None:
+                        return False
+                    cur = cur.kids[nextIdx]
+            return cur.end
+
+        return dfs(0, self.root)
